8e4428a93babd23d891b1e7497287b7762f9b8f1
Xiaobo Zhang <xzhang3@linkedin.com>
2019-07-15 19:33:05 -0700

    update app title

--- src/config/environment.dev.ts
+++ src/config/environment.dev.ts
@@ -2,17 +2,18 @@ import { LanguageType } from 'store/reducers/locale/langugeType'
 
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyAHOZ7rWGDODCwJMB3WIt63CAIa90qI-jg',
-    authDomain: 'test-4515a.firebaseapp.com',
-    databaseURL: 'https://test-4515a.firebaseio.com',
-    projectId: 'test-4515a',
-    storageBucket: 'test-4515a.appspot.com',
-    messagingSenderId: '964743099489'
+    apiKey: 'AIzaSyC2lWouJOchOjhfy-DUT8FgoihXhQcxTUk',
+    authDomain: 'groupslice-91395.firebaseapp.com',
+    databaseURL: 'https://groupslice-91395.firebaseio.com',
+    projectId: 'groupslice-91395',
+    storageBucket: 'groupslice-91395.appspot.com',
+    messagingSenderId: '89717345152',
+    appId: '1:289717345152:web:5a5b93c095c44be9'
   },
   settings: {
     enabledOAuthLogin: true,
     enabledOffline: true,
-    appName: 'Green',
+    appName: 'GroupSlice',
     defaultProfileCover: 'https://firebasestorage.googleapis.com/v0/b/open-social-33d92.appspot.com/o/images%2F751145a1-9488-46fd-a97e-04018665a6d3.JPG?alt=media&token=1a1d5e21-5101-450e-9054-ea4a20e06c57',
     defaultLanguage: LanguageType.English
   },
